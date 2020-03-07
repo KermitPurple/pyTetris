74f9e879992515805f316bc903fe48c86365d7e7
Shane McDonough <shanemcdman@gmail.com>
2020-03-06 17:41:19 -0800

    Impliment lock

--- coord.py
+++ coord.py
@@ -2,3 +2,8 @@ class coord:
     def __init__(self, x=0,y=0):
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        if self.x ==  other.x and self.y == other.y:
+            return True
+        return False
